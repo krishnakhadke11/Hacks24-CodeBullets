472d8c3588202d2e214048e95532b91582fe709c
vedjaiswal <vedjaiswal0506@gmail.com>
2024-01-31 16:10:31 +0530

    login signup added

--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -1,8 +1,81 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {TextField, Box, styled, FormControl, FilledInput, InputLabel, InputAdornment, IconButton, Button, Typography} from '@mui/material'
+import {Visibility, VisibilityOff} from '@mui/icons-material'
 
-function Login() {
+const Container = styled(Box)`
+  display: flex;
+  width : 400px;
+  justify-content: center;
+  align-items : center;
+  flex-direction : column;
+  gap : 1rem;
+  grid-area : right;
+  margin-left : 30px;
+  margin-top : 50px;
+`
+
+const SignUpButton = styled(Button)({
+    textTransform : "none",
+    backgroundColor : "#9667E0",
+    width:300,
+    height:45,
+    ":hover" :{
+      backgroundColor : "secondary"
+    }
+  })
+
+const SignInText = styled(Typography)({
+    color : "secondary",
+    fontSize : 14,
+    " > span" :{
+      cursor : "pointer",
+      textDecorationLine : "underline",
+    },
+    " > span:hover"  :{
+      color : "#040404",
+  }
+  })
+  
+  const PasswordInput = styled(FormControl)({
+    width:300
+  })
+  
+  const StyledTextField = styled(TextField)({
+    width : 300
+  });
+
+function Login({ setLogin }) {
+
+    const [showPassword, setShowPassword] = useState(false);
+  
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+    const onSignUpClick = () => {
+        setLogin(false);
+    }
+
   return (
-    <div>Login</div>
+    <Container>
+       <StyledTextField color='secondary' variant='filled' type='email' label='Username' />
+        <PasswordInput color='secondary' variant='filled'>
+            <InputLabel>Password</InputLabel>
+            <FilledInput
+            type={showPassword ? 'text' : 'password'}
+            endAdornment={
+                <InputAdornment position="end">
+                <IconButton
+                    onClick={handleClickShowPassword}
+                >
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+                </InputAdornment>
+            }
+            />
+        </PasswordInput>
+        {/* <Typography variant='caption'>Forget Password?</Typography> */}
+        <SignUpButton color='secondary' variant="contained">Login</SignUpButton>
+        <SignInText>New Here? <span onClick={onSignUpClick}>Create a new account</span></SignInText> 
+    </Container>
   )
 }
 
--- client/src/components/login/Register.js
+++ client/src/components/login/Register.js
@@ -1,8 +1,84 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {styled, Box} from '@mui/material'
+
+
+import Login from './Login'
+import SignUp from './SignUp'
+// import BackgroundImage from '../../images/sun-tornado.png'
+// import Logo from '../../images/inchat_logo.png'
+import loginImage from '../../images/loginPhoto.png'
+
+
+const Container = styled(Box)`
+ height : 100vh;
+ width : 100vw;
+ display : flex;
+ justify-content: center;
+ align-items : center;
+ background-size : cover;
+`
+const FormContainer = styled(Box)`
+  height : 600px;
+  width : 900px;
+  display: grid;
+  position : relative;
+  background-color : #FBFAFF;
+  border-radius : 20px;
+-webkit-box-shadow: 8px 8px 20px -3px rgba(4,4,4,1);
+-moz-box-shadow: 8px 8px 20px -3px rgba(4,4,4,1);
+box-shadow: 8px 8px 20px -3px rgba(4,4,4,1);
+  grid-template-areas : 
+    'logo logo'
+    'left right'
+    'left right';
+  
+`
+
+const LeftContainer = styled(Box)`
+  grid-area : left;
+  overflow : hidden;
+  border-radius : 0px 0px 0px 20px;
+  margin-left : 30px;
+`
+const Header = styled(Box)`
+  grid-area : logo;
+  height : 90px;
+`
+const HeaderImage = styled('img')({
+  marginLeft:'35%',
+  height: 120,
+  position:'absolute'
+})
+
+const LoginImage = styled('img')`
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+//   margin-top: -80px;
+//   transform: scale(1.5);
+  height : 400px;
+  width : 300px;
+  object-fit: cover;
+`
 
 function Register() {
+ 
+    const[login, setLogin] = useState(true);
+
   return (
-    <div>Register</div>
+    <Container>
+      <FormContainer>
+        <Header>LOGO
+          {/* <HeaderImage src={Logo} alt='logo' /> */}
+        </Header>
+        <LeftContainer>
+          <LoginImage autoPlay src={loginImage} alt="illustration" />
+        </LeftContainer>
+        <div>
+            {login ? <Login setLogin={setLogin}/> : <SignUp setLogin={setLogin}/>}
+        </div>
+      </FormContainer>
+    </Container>
   )
 }
 
