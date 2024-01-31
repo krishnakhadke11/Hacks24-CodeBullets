101bff724dbdd70e6b6ef530833c5d32c26754b3
vedjaiswal <vedjaiswal0506@gmail.com>
2024-01-31 18:41:16 +0530

    engagement

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } 
 import Layout from './components/Layout';
 import Home from './components/Home/Home';
 import Channels from './components/Channels/Channel'
+import Engagement from './components/engagement/Engagement';
 
 
 const router = createBrowserRouter(
@@ -9,6 +10,7 @@ const router = createBrowserRouter(
     <Route path='/' element={<Layout />}>
       <Route path='' element={<Home />} />
       <Route path='channels' element={<Channels />} />
+      <Route path='engagement' element={<Engagement/>}/>
     </Route>
   )
 )
---
+++ client/src/components/engagement/Engagement.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Engagement() {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Engagement
